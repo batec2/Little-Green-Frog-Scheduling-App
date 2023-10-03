bd3ab22ac4ec2526fa2a6b85a969ea207309af67
D. Fender <26173007+postfen@users.noreply.github.com>
2023-10-02 22:40:58 -0600

    Employee Entry now supports tab/enter on text entry fields.

--- app/src/main/java/com/example/f23hopper/ui/employee/EmployeeEntryScreen.kt
+++ app/src/main/java/com/example/f23hopper/ui/employee/EmployeeEntryScreen.kt
@@ -1,23 +1,15 @@
 package com.example.f23hopper.ui.employee
 
-import EmployeeRepository
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -30,48 +22,42 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.f23hopper.data.EmployeeDao
-import com.example.f23hopper.data.EmployeesDatabase
-import com.example.f23hopper.ui.AppViewModelProvider
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEvent
 import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.onPreviewKeyEvent
 import androidx.compose.ui.input.key.type
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.f23hopper.data.ShiftType
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.f23hopper.data.shifttype.ShiftType
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun EmployeeEntryScreen(
-    viewModel: EmployeeEntryViewModel = viewModel(factory = AppViewModelProvider.Factory)
-){
+fun EmployeeEntryScreen() {
     val coroutineScope = rememberCoroutineScope()
+    val viewModel = hiltViewModel<EmployeeEntryViewModel>()
     EmployeeEntryBody(
         employeeUiState = viewModel.employeeUiState,
         employeeDetails = viewModel.employeeUiState.employeeDetails,
         onEmployeeValueChange = viewModel::updateUiState,
         onSaveClick = {
-            coroutineScope.launch{
+            coroutineScope.launch {
                 viewModel.saveEmployee()
             }
         },
     )
 }
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EmployeeEntryBody(
@@ -79,19 +65,19 @@ fun EmployeeEntryBody(
     employeeDetails: EmployeeDetails,
     onEmployeeValueChange:(EmployeeDetails) -> Unit,
     onSaveClick: () -> Unit
-){
+) {
     Scaffold(
         topBar = {
             TopAppBar(title = {})
         }
-    ){innerPadding->
+    ) { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
                 .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
-        ){
+        ) {
             EmployeeInfo(
                 onEmployeeInfoChange = onEmployeeValueChange,
                 employeeDetails = employeeDetails
@@ -103,7 +89,7 @@ fun EmployeeEntryBody(
             Button(
                 onClick = onSaveClick,
                 enabled = employeeUiState.isEmployeeValid,
-                ) {
+            ) {
                 Text(text = "Add")
             }
         }
@@ -119,64 +105,39 @@ fun EmployeeInfo(
 
     val focusManager = LocalFocusManager.current
 
-    OutlinedTextField(
-        modifier = Modifier.onPreviewKeyEvent {
-            when {
-                KeyEventType.KeyUp == it.type && Key.Tab == it.key -> {
-                    focusManager.moveFocus(FocusDirection.Next)
-                    true
-                }
-
-                else -> false
+    val handleKeyEvent: (KeyEvent) -> Boolean = {
+        when {
+            (it.type == KeyEventType.KeyDown && (it.key == Key.Tab || it.key == Key.Enter)) -> {
+                focusManager.moveFocus(FocusDirection.Next)
+                true
             }
-        },
+            else -> false
+        }
+    }
+
+    //TODO: Refactor out this repetition. Structs for the field, for loop of structs into a component?
+    OutlinedTextField(
+        modifier = Modifier.onPreviewKeyEvent(handleKeyEvent),
         value = employeeDetails.firstName,
-        onValueChange = {onEmployeeInfoChange(employeeDetails.copy(firstName=it))},
+        onValueChange = { onEmployeeInfoChange(employeeDetails.copy(firstName = it)) },
         label = { Text("First Name") }
     )
     OutlinedTextField(
-        modifier = Modifier.onPreviewKeyEvent {
-            when {
-                KeyEventType.KeyUp == it.type && Key.Tab == it.key -> {
-                    focusManager.moveFocus(FocusDirection.Next)
-                    true
-                }
-
-                else -> false
-            }
-        },
+        modifier = Modifier.onPreviewKeyEvent(handleKeyEvent),
         value = employeeDetails.lastName,
-        onValueChange = {onEmployeeInfoChange(employeeDetails.copy(lastName=it))},
+        onValueChange = { onEmployeeInfoChange(employeeDetails.copy(lastName = it)) },
         label = { Text("Last Name") }
     )
     OutlinedTextField(
-        modifier = Modifier.onPreviewKeyEvent {
-            when {
-                KeyEventType.KeyUp == it.type && Key.Tab == it.key -> {
-                    focusManager.moveFocus(FocusDirection.Next)
-                    true
-                }
-
-                else -> false
-            }
-        },
+        modifier = Modifier.onPreviewKeyEvent(handleKeyEvent),
         value = employeeDetails.email,
-        onValueChange = {onEmployeeInfoChange(employeeDetails.copy(email=it))},
+        onValueChange = { onEmployeeInfoChange(employeeDetails.copy(email = it)) },
         label = { Text("Email") }
     )
     OutlinedTextField(
-        modifier = Modifier.onPreviewKeyEvent {
-            when {
-                KeyEventType.KeyDown == it.type && Key.Tab == it.key -> {
-                    focusManager.moveFocus(FocusDirection.Next)
-                    true
-                }
-
-                else -> false
-            }
-        },
+        modifier = Modifier.onPreviewKeyEvent(handleKeyEvent),
         value = employeeDetails.phoneNumber,
-        onValueChange = {onEmployeeInfoChange(employeeDetails.copy(phoneNumber=it))},
+        onValueChange = { onEmployeeInfoChange(employeeDetails.copy(phoneNumber = it)) },
         label = { Text("Phone Number") }
     )
 }
@@ -191,9 +152,9 @@ fun WeekendSelector(
     Row(
         horizontalArrangement = Arrangement.SpaceEvenly,
         verticalAlignment = Alignment.CenterVertically
-    ){
-        Text(text="Opening")
-        Spacer(modifier = Modifier.padding(start=5.dp))
+    ) {
+        Text(text = "Opening")
+        Spacer(modifier = Modifier.padding(start = 5.dp))
         Switch(
             checked = checkedOpen,
             onCheckedChange = {
@@ -201,9 +162,9 @@ fun WeekendSelector(
                 onWeekendValueChange(employeeDetails.copy(canOpen = it))
             },
         )
-        Spacer(modifier = Modifier.padding(start=5.dp))
-        Text(text="Closing")
-        Spacer(modifier = Modifier.padding(start=5.dp))
+        Spacer(modifier = Modifier.padding(start = 5.dp))
+        Text(text = "Closing")
+        Spacer(modifier = Modifier.padding(start = 5.dp))
         Switch(
             checked = checkedClose,
             onCheckedChange = {
@@ -217,21 +178,21 @@ fun WeekendSelector(
 
 @Composable
 fun ScheduleSelector(
-    day:String,
+    day: String,
     employeeDetails: EmployeeDetails,
     onSelectionChange: (EmployeeDetails) -> Unit = {},
 
-){
+    ) {
     var sliderPosition by remember { mutableStateOf(0f) }
     var text by remember { mutableStateOf("") }
     Column {
         Row(
             modifier = Modifier,
-                //.width(50.dp)
+            //.width(50.dp)
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceEvenly,
-        ){
-            Text(text=day)
+        ) {
+            Text(text = day)
             Slider(
                 value = sliderPosition,
                 onValueChange = {},
@@ -241,14 +202,10 @@ fun ScheduleSelector(
                 steps = 1,
                 valueRange = 0f..2f
             )
-            if(sliderPosition == 0f){
-                text = ShiftType.DAY.toString()
-            }
-            else if(sliderPosition == 1f){
-                text = ShiftType.FULL.toString()
-            }
-            else{
-                text = ShiftType.NIGHT.toString()
+            text = when (sliderPosition) {
+                0f -> ShiftType.DAY.toString()
+                1f -> ShiftType.FULL.toString()
+                else -> ShiftType.NIGHT.toString()
             }
         }
         Text(text = text)
