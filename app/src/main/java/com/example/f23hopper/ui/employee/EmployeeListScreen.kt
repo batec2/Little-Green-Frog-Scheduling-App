cc610ec9f3ff93302a655a5760598855a302aeba
D. Fender <26173007+postfen@users.noreply.github.com>
2023-10-04 23:41:27 -0600

    Refactored ScheduleSelector to use a for loop for each day, converted ifs to whens, changed icon and box styling.

--- app/src/main/java/com/example/f23hopper/ui/employee/EmployeeListScreen.kt
+++ app/src/main/java/com/example/f23hopper/ui/employee/EmployeeListScreen.kt
@@ -3,8 +3,6 @@ package com.example.f23hopper.ui.employee
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -18,17 +16,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.asFlow
 
 @Composable
@@ -64,7 +62,7 @@ fun EmployeeListScreen(
                 }
 
                 FloatingActionButton(
-                    containerColor = colorScheme.primary.copy(alpha = 0.8f),
+                    containerColor = colorScheme.primary,
                     contentColor = colorScheme.onTertiaryContainer,
                     onClick = { navigateToEmployeeAdd() },
                     modifier = Modifier.align(Alignment.BottomEnd).padding(16.dp)
@@ -72,7 +70,7 @@ fun EmployeeListScreen(
                     Icon(
                         Icons.Default.Add,
                         contentDescription = "add",
-                        tint = colorScheme.onPrimary.copy(alpha = 0.8f)
+                        tint = colorScheme.onPrimary
                     )
                 }
             }
