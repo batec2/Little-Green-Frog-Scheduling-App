0a1527cc3f18670098abecb35e770ca49c647dc7
Crush Bate <50185023+batec2@users.noreply.github.com>
2023-10-09 16:36:40 -0600

    Initial work on look for list

--- app/src/main/java/com/example/f23hopper/ui/employee/EmployeeListScreen.kt
+++ app/src/main/java/com/example/f23hopper/ui/employee/EmployeeListScreen.kt
@@ -1,15 +1,20 @@
 package com.example.f23hopper.ui.employee
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
@@ -22,6 +27,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -31,8 +37,11 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.asFlow
 import com.example.f23hopper.data.employee.Employee
+import com.example.f23hopper.data.shifttype.ShiftType
 import com.example.f23hopper.ui.icons.rememberLock
 import com.example.f23hopper.ui.icons.rememberLockOpenRight
+import com.example.f23hopper.ui.icons.rememberPartlyCloudyNight
+import com.example.f23hopper.ui.icons.rememberWbSunny
 
 @Composable
 fun EmployeeListScreen(
@@ -85,27 +94,64 @@ fun EmployeeListItem(
                     .padding(16.dp)
             ) {
                 Column{
-                    Row {
+                    Row (
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
                         Text(
                             "${employee.employeeId}: ${employee.firstName + ' ' + employee.lastName}",
-                            style = TextStyle(fontSize = 15.sp)
+                            style = TextStyle(fontSize = 20.sp)
                         )
                         if(employee.canOpen){
-                            Icon(imageVector = rememberLockOpenRight(), contentDescription = "Can Open")
+                            Icon(imageVector = rememberLockOpenRight(),
+                                modifier = Modifier.size(20.dp),
+                                contentDescription = "Can Open")
                         }
                         if(employee.canClose){
-                            Icon(imageVector = rememberLock(), contentDescription = "Can Open")
+                            Icon(imageVector = rememberLock(),
+                                modifier = Modifier.size(20.dp),
+                                contentDescription = "Can Open")
                         }
                     }
                     Row{
-                        val week = listOf<String>(employee.monday.toString(),
-                            employee.tuesday.toString(),
-                            employee.wednesday.toString(),
-                            employee.thursday.toString(),
-                            employee.friday.toString())
+                        val week = listOf(
+                            Pair(employee.sunday,"S"),
+                            Pair(employee.monday,"M"),
+                            Pair(employee.tuesday,"T"),
+                            Pair(employee.wednesday,"W"),
+                            Pair(employee.thursday,"R"),
+                            Pair(employee.friday,"F"),
+                            Pair(employee.sunday,"S"))
 
-                        week.forEach { 
-                            week -> Text(text = week.first().toString())
+                        Row(
+                            modifier = Modifier
+                                .weight(1f)
+                                .clip(RoundedCornerShape(5.dp))
+                                .background(color = MaterialTheme.colorScheme.secondaryContainer),
+                            horizontalArrangement = Arrangement.SpaceEvenly
+                        ){
+                            Icon(imageVector = rememberWbSunny(),
+                                modifier = Modifier.size(20.dp),
+                                contentDescription = "Day Shift")
+                            week.forEach {
+                                    week -> Text(text = if(week.first==ShiftType.DAY||week.first==ShiftType.FULL) week.second else "",
+                                color = Color.DarkGray)
+                            }
+                        }
+                        Spacer(modifier = Modifier.size(5.dp))
+                        Row(
+                            modifier = Modifier
+                                .weight(1f)
+                                .clip(RoundedCornerShape(5.dp))
+                                .background(color = MaterialTheme.colorScheme.secondaryContainer),
+                            horizontalArrangement = Arrangement.SpaceEvenly
+                        ){
+                            Icon(imageVector = rememberPartlyCloudyNight(),
+                                modifier = Modifier.size(20.dp),
+                                contentDescription = "Night Shift")
+                            week.forEach {
+                                    week -> Text(text = if(week.first==ShiftType.NIGHT||week.first==ShiftType.FULL) week.second else " ",
+                                color = Color.DarkGray)
+                            }
                         }
                     }
                 }
