f8015299f80c4e36b89a7428f6148b404e0ffe1c
D. Fender <26173007+postfen@users.noreply.github.com>
2023-10-14 11:16:39 -0600

    Fixed text on EmployeeListScreen (added dark mode compat)

--- app/src/main/java/com/example/f23hopper/ui/employee/EmployeeListScreen.kt
+++ app/src/main/java/com/example/f23hopper/ui/employee/EmployeeListScreen.kt
@@ -18,19 +18,14 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.colorScheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -38,9 +33,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -225,7 +218,7 @@ fun ListScheduleInfo(
             modifier = Modifier
                 .weight(1f)
                 .clip(RoundedCornerShape(5.dp))
-                .background(color = MaterialTheme.colorScheme.secondaryContainer),
+                .background(color = colorScheme.secondaryContainer),
             horizontalArrangement = Arrangement.SpaceEvenly
         ){
             Icon(imageVector = rememberWbSunny(),
@@ -233,7 +226,7 @@ fun ListScheduleInfo(
                 contentDescription = "Day Shift")
             week.forEach {
                     week -> Text(text = if(week.first==ShiftType.DAY||week.first==ShiftType.FULL) week.second else "",
-                color = Color.DarkGray)
+                color = colorScheme.onSecondaryContainer)
             }
         }
         Spacer(modifier = Modifier.size(5.dp))
@@ -241,7 +234,7 @@ fun ListScheduleInfo(
             modifier = Modifier
                 .weight(1f)
                 .clip(RoundedCornerShape(5.dp))
-                .background(color = MaterialTheme.colorScheme.secondaryContainer),
+                .background(color = colorScheme.secondaryContainer),
             horizontalArrangement = Arrangement.SpaceEvenly
         ){
             Icon(imageVector = rememberPartlyCloudyNight(),
@@ -249,7 +242,7 @@ fun ListScheduleInfo(
                 contentDescription = "Night Shift")
             week.forEach {
                     week -> Text(text = if(week.first==ShiftType.NIGHT||week.first==ShiftType.FULL) week.second else " ",
-                color = Color.DarkGray)
+                 color = colorScheme.onSecondaryContainer)
             }
         }
     }
