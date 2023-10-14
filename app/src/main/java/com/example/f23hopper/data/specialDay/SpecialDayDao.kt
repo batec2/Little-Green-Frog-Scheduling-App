13b9747e2e6670d67a61a0b28be6d3f3613d5319
mitchelnelson <mitchelnelson@live.ca>
2023-10-14 12:31:04 -0600

    create DAO and Repo for specialDay

---
+++ app/src/main/java/com/example/f23hopper/data/specialDay/SpecialDayDao.kt
@@ -0,0 +1,25 @@
+package com.example.f23hopper.data.specialDay
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+import java.sql.Date
+
+@Dao
+interface SpecialDayDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(specialDay: SpecialDay): Date  // Return the Date ID
+
+    @Update
+    suspend fun update(specialDay: SpecialDay)
+
+    @Delete
+    suspend fun delete(specialDay: SpecialDay)
+
+    @Query("SELECT * FROM specialdays")
+    fun getSpecialDays(): Flow<SpecialDay>
+
+    @Query("DELETE FROM specialdays")
+    suspend fun deleteAllSpecialDays()
+
+}
