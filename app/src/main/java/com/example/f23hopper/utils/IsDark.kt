10329e914ee2039027f76255a9a04cb0d704c582
D. Fender <26173007+postfen@users.noreply.github.com>
2023-10-16 10:25:13 -0600

    - Deleted unneeded file/func

--- app/src/main/java/com/example/f23hopper/utils/IsDark.kt
+++
@@ -1,7 +0,0 @@
-package com.example.f23hopper.utils
-
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.runtime.Composable
-
-@Composable
-fun isDark() = isSystemInDarkTheme()
