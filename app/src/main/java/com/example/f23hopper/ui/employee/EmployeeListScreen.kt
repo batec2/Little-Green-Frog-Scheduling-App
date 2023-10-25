3fec7145c5698b709dd69fad1b1291c193029b89
D. Fender <26173007+postfen@users.noreply.github.com>
2023-10-25 11:59:43 -0600

    - Deleted FAB, minor tweaks
    - Started IsValid function (just header, really).

--- app/src/main/java/com/example/f23hopper/ui/employee/EmployeeListScreen.kt
+++ app/src/main/java/com/example/f23hopper/ui/employee/EmployeeListScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -28,7 +27,6 @@ import androidx.compose.material3.DismissValue
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.colorScheme
@@ -48,8 +46,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -139,32 +135,7 @@ fun EmployeeListScreen(
                     navigateToEmployeeEdit()
                 }
             }
-        }, floatingActionButton = {
-
-            FloatingActionButton(
-                containerColor = colorScheme.primary.copy(),
-                onClick = { navigateToEmployeeAdd() },
-                modifier = Modifier
-                    .graphicsLayer(
-                        translationX = offsetX,
-                        translationY = offsetY
-                    )
-                    .pointerInput(Unit) {
-                        detectDragGestures { change, dragAmount ->
-                            change.consume()
-                            offsetX += dragAmount.x
-                            offsetY += dragAmount.y
-                        }
-                    }
-                    .padding(16.dp)
-            ) {
-                Icon(
-                    Icons.Default.Add,
-                    contentDescription = "add",
-                    tint = colorScheme.onPrimary.copy()
-                )
-            }
-        }
+        },
     )
 }
 
