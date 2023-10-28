16e3a2af26103bad50c292befa1dea4cf81efd84
D. Fender <26173007+postfen@users.noreply.github.com>
2023-10-28 16:01:29 -0600

    - fix: added text overflow options to EmployeeList

--- app/src/main/java/com/example/f23hopper/ui/shiftedit/ShiftEditScreen.kt
+++ app/src/main/java/com/example/f23hopper/ui/shiftedit/ShiftEditScreen.kt
@@ -40,6 +40,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -186,7 +187,6 @@ private fun LazyListScope.addShiftTypeSection(
         }
     }
 }
-
 
 
 @Composable
@@ -363,7 +363,9 @@ fun EmployeeList(
             Text(
                 text = displayName,
                 style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier.weight(1f)
+                modifier = Modifier.weight(1f),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
             )
 
             Text(
