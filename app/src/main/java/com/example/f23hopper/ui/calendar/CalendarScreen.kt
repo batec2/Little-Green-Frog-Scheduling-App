ec671febe22fa3eafc2243a8eea53382dc2675d8
Crush Bate <50185023+batec2@users.noreply.github.com>
2023-10-28 23:15:15 -0600

    bottom bar page indicator

--- app/src/main/java/com/example/f23hopper/ui/calendar/CalendarScreen.kt
+++ app/src/main/java/com/example/f23hopper/ui/calendar/CalendarScreen.kt
@@ -1,4 +1,4 @@
-@file:OptIn(ExperimentalFoundationApi::class)
+@file:OptIn(ExperimentalFoundationApi::class, ExperimentalFoundationApi::class)
 
 package com.example.f23hopper.ui.calendar
 
@@ -19,8 +19,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Create
@@ -38,7 +40,9 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -170,20 +174,35 @@ fun Calendar(
                 WeekDays(modifier = Modifier.padding(vertical = 8.dp))
             },
         )
-//        Divider(color = itemBackgroundColor)
-        if(selection != null){
-            HorizontalPager(pageCount = 2) {page->
+        Column (
+            modifier = Modifier.fillMaxSize()
+        ){
+    //        Divider(color = itemBackgroundColor)
+            val pagerState = rememberPagerState(initialPage = 0)
+            HorizontalPager(pageCount = 2,state = pagerState, modifier = Modifier.weight(1f)) {page->
                 if(page==0){
-                    Divider(color = itemBackgroundColor)
-                    val isSpecialDay = specialDaysByDay[selection?.date!!] != null
-                    ShiftDetailsForDay(
-                        shiftsOnSelectedDate,
-                        selection?.date!!,
-                        isSpecialDay = isSpecialDay,
-                        navigateToShiftView
-                    )
+                    if(selection !=null){
+                        Divider(color = itemBackgroundColor)
+                        val isSpecialDay = specialDaysByDay[selection?.date!!] != null
+                        ShiftDetailsForDay(
+                            shiftsOnSelectedDate,
+                            selection?.date!!,
+                            isSpecialDay = isSpecialDay,
+                            navigateToShiftView
+                        )
+                    }
+                    else{
+                        Row (
+                            modifier = Modifier.fillMaxSize(),
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically
+
+                        ){
+                            Text(text = "Select Day")
+                        }
+                    }
                 }
-                else{
+                else if(page ==1){
                     Row (
                         modifier = Modifier.fillMaxSize(),
                         horizontalArrangement = Arrangement.Center,
@@ -194,9 +213,25 @@ fun Calendar(
                     }
                 }
             }
+            Row(
+                Modifier
+                    //.wrapContentHeight()
+                    .fillMaxWidth()
+                    .padding(bottom = 2.dp).weight(0.1f),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                repeat(2) { iteration ->
+                    val color = if (pagerState.currentPage == iteration) Color.DarkGray else Color.LightGray
+                    Box(
+                        modifier = Modifier
+                            .padding(2.dp)
+                            .clip(CircleShape)
+                            .background(color)
+                            .size(5.dp)
+                    )
+                }
+            }
         }
-
-
     }
 }
 
