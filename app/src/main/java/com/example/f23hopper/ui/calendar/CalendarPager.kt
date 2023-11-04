bd71b03f7ed6ea9480e4d211e79da88d3fd5761b
D. Fender <26173007+postfen@users.noreply.github.com>
2023-11-04 08:47:49 -0600

    - fix: restoring color function to background of ShiftRow

--- app/src/main/java/com/example/f23hopper/ui/calendar/CalendarPager.kt
+++ app/src/main/java/com/example/f23hopper/ui/calendar/CalendarPager.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -357,23 +356,7 @@ fun ShiftRowEmployeeEntry(
         modifier = modifier
             .fillMaxWidth()
             .padding(2.dp)
-            .background(MaterialTheme.colorScheme.secondaryContainer)
-            .clickable { onEmployeeClick(shift.employee) }
-            .background(
-                if(shift.schedule.shiftType == ShiftType.DAY) {
-                    if(isSystemInDarkTheme()) {
-                        Color(0xFF72B9E0) //Day Dark
-                    } else {
-                        Color(0xFFB1C1F2) //Day Light
-                    }
-                } else {
-                    if(isSystemInDarkTheme()) {
-                        Color(0xFF2C8D76) //Evening Dark
-                    } else {
-                        Color(0xFFA18AB4) //Evening Light
-                    }
-                }
-            )
+            .background(getShiftRowColor(shiftType = shift.schedule.shiftType))
             .clickable { onEmployeeClick(shift.employee) },
         horizontalArrangement = Arrangement.Start,
         verticalAlignment = Alignment.CenterVertically
